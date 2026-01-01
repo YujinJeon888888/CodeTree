9b01db249339765a9730314a536a574876886582
Jeon Yujin <117812553+YujinJeon888888@users.noreply.github.com>
2026-01-02 02:11:48 +0900

    Title: 1이 되는 순간까지, Time: 225ms, Memory: 80MB, Status: Memory Limit Exceed - Codetree

--- 260102/1이 되는 순간까지/until-the-moment-I-reach-one.cpp
+++ 260102/1이 되는 순간까지/until-the-moment-I-reach-one.cpp
@@ -1,33 +1,33 @@
 #include <iostream>
-#include <cmath>
 
 using namespace std;
 
 int N;
 
-
-//나눈 횟수를 반환하는 함수(1일땐 나누지x)
-int Num(int N){
-    //종료조건: 1이면 0반환
-    if(N==1){
+//나눈 횟수를 구하는 재귀함수
+int DivideCount(int N){
+    //종료: 1이면 나눌 수 x == 나눈횟수 0
+    if(N<=1){
         return 0;
     }
 
-    //홀수일 때
-    if(N&1==1){
-        //N/3를 나눈 횟수 + 지금 나누기 실행했으므로 1
-        return Num(N/3) + 1;
+    //홀수
+    if(N&1){
+        //나눌횟수 + 지금 나눈 횟수
+        DivideCount(N/3)+1;
     }
-    //짝수일 때
+    //짝수
     else{
-        //N/2를 나눈 횟수 + 지금 나누기 실행했으므로 1
-        return Num(N/2) + 1;
+        //나눌횟수 + 지금 나눈 횟수
+        DivideCount(N/2)+1;
     }
 }
 
+
 int main() {
     cin >> N;
-    
-    cout<<Num(N);
+
+    // Please write your code here.
+    cout<<DivideCount(N);
     return 0;
 }
