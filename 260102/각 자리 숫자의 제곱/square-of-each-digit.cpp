59fcb3b1ba954fcc894113607f831cd9c7c147ae
Jeon Yujin <117812553+YujinJeon888888@users.noreply.github.com>
2026-01-02 01:28:57 +0900

    Title: 각 자리 숫자의 제곱, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260102/각 자리 숫자의 제곱/square-of-each-digit.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+//함수: 각 자리수의 제곱의 합을 출력
+void SumOfEachSquare(int num){
+    //종료조건: 한자리수면 => num의 제곱을 리턴
+    if(num%10==num){
+        return num*num;
+    }
+
+    //일반화: F(10으로 나눈 몫) + F(10으로 나눈 나머지)
+    return SumOfEachSquare(num/10) + SumOfEachSquare(num%10);
+}
+
+int main() {
+    cin >> N;
+
+    // Please write your code here.
+    cout<<SumOfEachSquare(N);
+
+    return 0;
+}
