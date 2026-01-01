a537048aae9f2ea74d5bd64406f419741a1ed027
Jeon Yujin <117812553+YujinJeon888888@users.noreply.github.com>
2026-01-02 01:58:42 +0900

    Title: 1이 되는 순간까지, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260102/1이 되는 순간까지/until-the-moment-I-reach-one.cpp
+++ 260102/1이 되는 순간까지/until-the-moment-I-reach-one.cpp
@@ -6,12 +6,28 @@ using namespace std;
 int N;
 
 
+//나눈 횟수를 반환하는 함수(1일땐 나누지x)
+int Num(int N){
+    //종료조건: 1이면 0반환
+    if(N==1){
+        return 0;
+    }
 
+    //짝수일 때
+    if(N&1==0){
+        //N/2를 나눈 횟수 + 지금 나누기 실행했으므로 1
+        return Num(N/2) + 1;
+    }
+    //홀수일 때
+    else{
+        //N/3를 나눈 횟수 + 지금 나누기 실행했으므로 1
+        return Num(N/3) + 1;
+    }
+}
 
 int main() {
     cin >> N;
     
-    cout<<(int)log2(N)-1;
-
+    cout<<Num(N);
     return 0;
 }
