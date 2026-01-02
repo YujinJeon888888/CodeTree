e2602b2362f91052f4d5c9c9676b11a7b39dd788
Jeon Yujin <117812553+YujinJeon888888@users.noreply.github.com>
2026-01-02 17:37:49 +0900

    Title: 홀수 짝수에 따른 출력값, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

--- 260102/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.cpp
+++ 260102/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.cpp
@@ -4,38 +4,43 @@ using namespace std;
 
 int N;
 
-//1또는 2부터 N까지의 합을 출력하는 함수
-int Sum(int n){
+//1부터 N까지의 합을 출력하는 함수
+int SumOdd(int n){
     //종료조건
- 
-    if(n<=2){
-        //홀수
-        if(n&1){
-            return 1;
-        }
-        //짝수
-        else{
-            return 2;
-        }
+    if(n<=1){
+        return 1;
     }
     
 
     //일반화
-    //홀수
-    if(n&1){
-        return Sum(n-1) + n;
-    }
-    //짝수
-    else{
-        return Sum(n-1) + n;
-    }
+    return Sum(n-1) + n;
 }
+
+//2부터 N까지의 합을 출력하는 함수
+int SumEven(int n){
+    //종료조건
+    if(n<=2){
+        return 2;
+    }
+    
+
+    //일반화
+    return Sum(n-1) + n;
+}
+
+
+
 
 
 int main() {
     cin >> N;
 
     // Please write your code here.
-    cout<<Sum(N);
+    if(N&1){
+        cout<<SumOdd(N);
+    }
+    else{
+        cout<<SumEven(N);
+    }
     return 0;
 }
