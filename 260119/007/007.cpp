3cab256f0b6f0ca01471080abbc3184349c71e97
Jeon Yujin <117812553+YujinJeon888888@users.noreply.github.com>
2026-01-19 20:43:16 +0900

    Title: 007, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

--- 260119/007/007.cpp
+++ 260119/007/007.cpp
@@ -3,18 +3,28 @@
 #include <tuple>
 using namespace std;
 
-string secret_code;
-char meeting_point;
-int meeting_time;
+class Info{
+    public:
+        string secret_code;
+        char meeting_point;
+        int meeting_time;
+
+        Info(string secret_code, char meeting_point, int meeting_time){
+            this->secret_code = secret_code;
+            this->meeting_point = meeting_point;
+            this->meeting_time = meeting_time;
+        }
+}
 
 int main() {
-    cin >> secret_code >> meeting_point >> meeting_time;
+    Info info = new Info();
+
+    cin >> info.secret_code >> info.meeting_point >> info.meeting_time;
 
     // Please write your code here.
-    tuple<string,char,int> t = make_tuple(secret_code,meeting_point,meeting_time);
-    cout<<"secret code : "<<get<0>(t)<<'\n';
-    cout<<"meeting point : "<<get<1>(t)<<'\n';
-    cout<<"time : "<<get<2>(t)<<'\n';
+    cout<<"secret code : "<<info.secret_code<<"\n";
+    cout<<"meeting point : "<<info.meeting_point<<"\n";
+    cout<<"time : "<<info.meeting_time<<'\n';
 
     return 0;
 }
