bd589405199f39d6a69742e62a558218586e554d
Jeon Yujin <117812553+YujinJeon888888@users.noreply.github.com>
2026-01-20 18:44:34 +0900

    Title: 총점 비교, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260120/총점 비교/compare-total-points.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+class Student{
+    string name;
+    int score1;
+    int score2;
+    int score3;
+}
+
+bool cmp(const Student& a, const Student& b){
+    return a.score1+a.score2+a.score3<b.score1+b.score2+b.score3;
+}
+
+int main() {
+    cin >> n;
+    vector<Student> students;
+
+    for (int i = 0; i < n; i++) {
+        cin >> students[i].name;
+        cin >> students[i].score1;
+        cin >> students[i].score2;
+        cin >> students[i].score3;
+    }
+
+    // Please write your code here.
+    sort(students.begin(),students.end(),cmp);
+
+    for(const auto& val: students){
+        cout<<val.name<<' '<<val.score1<<' '<<val.score2<<' '<<val.score3<<'\n';
+    }
+    return 0;
+}
