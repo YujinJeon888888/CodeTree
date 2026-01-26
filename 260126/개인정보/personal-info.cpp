fb9fc7144c1b8c64ee80c0cb1b283c72e4c035e7
Jeon Yujin <117812553+YujinJeon888888@users.noreply.github.com>
2026-01-26 15:29:42 +0900

    Title: 개인정보, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260126/개인정보/personal-info.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Student{
+    public:
+        string name;
+        int height;
+        double weight;
+};
+
+bool cmpByName(const Student& a, const Student& b){
+    return a.name<b.name;
+}
+
+bool cmpByHeight(const Student& a, const Student& b){
+    return a.height>b.height;
+}
+
+int main() {
+    vector<Student> students(5);
+    for (int i = 0; i < 5; i++) {
+        cin >> students[i].name >> students[i].height >> students[i].weight;
+    }
+
+    // Please write your code here.
+    sort(students.begin(),students.end(),cmpByName);
+    cout<<"name"<<'\n';
+    for(int i=0;i<5;i++){
+        cout<<students[i].name<<' '<<students[i].height<<' '<<students[i].weight<<'\n';
+    }
+    cout<<'\n';
+
+    sort(students.begin(),students.end(),cmpByHeight);
+    cout<<"height"<<'\n';
+    for(int i=0;i<5;i++){
+        cout<<students[i].name<<' '<<students[i].height<<' '<<students[i].weight<<'\n';
+    }
+
+    return 0;
+}
