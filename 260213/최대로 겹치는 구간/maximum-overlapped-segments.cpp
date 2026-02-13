51339aa0b1543999257a3e76a7f933a21d7d2ed5
Jeon Yujin <117812553+YujinJeon888888@users.noreply.github.com>
2026-02-13 18:42:52 +0900

    Title: 최대로 겹치는 구간, Time: 8ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 260213/최대로 겹치는 구간/maximum-overlapped-segments.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int n;
+int x1[100], x2[100];
+
+int main() {
+    cin >> n;
+    vector<int> line(200,0);
+    int max=-1;
+    for (int i = 0; i < n; i++) {
+        cin >> x1[i] >> x2[i];
+        
+        for(int j=x1[i];j<x2[i];j++){
+            line[j]+=1;
+        }
+    }
+
+    // Please write your code here.
+    for(int i=0;i<200;i++){
+        if(max<line[i]){
+            max=line[i];
+        }
+    }
+    cout<<max;
+
+    return 0;
+}
