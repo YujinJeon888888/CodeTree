9fed0a8712e9b2df3a75fea3410a66bff292361c
Jeon Yujin <117812553+YujinJeon888888@users.noreply.github.com>
2026-02-13 16:31:46 +0900

    Title: 2진수로 변환하기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260213/2진수로 변환하기/convert-to-binary.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int n;
+
+int main() {
+    cin >> n;
+
+    // Please write your code here.
+    // 벡터 초기화
+    vector<int> binary;
+    int index=0;
+    binary.reserve(50000);
+    // 종료컨디션: n<2이면 n넣고 종료
+    while(n>2){
+        // n/2해가며, 2로 나눈 나머지를 vector[index++]에 저장
+        binary[index++] = n%2;
+        n /= 2;
+    }
+    binary[index] = n;
+
+    //출력 (인덱스 줄어들면서 출력)
+    for(int i=index;i>=0;i--){
+        cout<<binary[i];
+    }
+    return 0;
+}
