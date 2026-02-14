7a4bded58fa94edb8cba575f12c5340a0ceebaa8
Jeon Yujin <117812553+YujinJeon888888@users.noreply.github.com>
2026-02-14 16:25:25 +0900

    Title: 왔다 갔던 구역 2, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260214/왔다 갔던 구역 2/area-been-to-and-from2.cpp
+++ 260214/왔다 갔던 구역 2/area-been-to-and-from2.cpp
@@ -9,8 +9,8 @@ char dir[100];
 
 int main() {
     cin >> n;
-    vector<int> line(100,0);
-    int index=50;
+    vector<int> line(2000,0);
+    int index=1000;
     int answer=0;
     for (int i = 0; i < n; i++) {
         cin >> x[i] >> dir[i];
@@ -24,7 +24,7 @@ int main() {
         }
     }
 
-    for(int i=0;i<100;i++){
+    for(int i=0;i<2000;i++){
         if(line[i]>1){
             answer+=1;
         }
