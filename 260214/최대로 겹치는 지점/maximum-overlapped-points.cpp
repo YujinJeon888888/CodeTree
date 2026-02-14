16546f3c9acb020a7cd6049cf4a75fe80b36a88c
Jeon Yujin <117812553+YujinJeon888888@users.noreply.github.com>
2026-02-14 15:53:07 +0900

    Title: 최대로 겹치는 지점, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260214/최대로 겹치는 지점/maximum-overlapped-points.cpp
+++ 260214/최대로 겹치는 지점/maximum-overlapped-points.cpp
@@ -8,8 +8,9 @@ int x1[100], x2[100];
 
 int main() {
     cin >> n;
-    vector<int> line(n,0);
+    vector<int> line(100+1,0);
     int max=-1;
+
     for (int i = 0; i < n; i++) {
         cin >> x1[i] >> x2[i];
         for(int j=x1[i];j<=x2[i];j++){
