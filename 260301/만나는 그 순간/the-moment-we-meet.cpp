fc24e74962db696c9343f3aca1ebe0ed3a87e3ae
Jeon Yujin <117812553+YujinJeon888888@users.noreply.github.com>
2026-03-01 21:24:48 +0900

    Title: 만나는 그 순간, Time: 18ms, Memory: 0MB, Status: Runtime Error - Codetree

--- 260301/만나는 그 순간/the-moment-we-meet.cpp
+++ 260301/만나는 그 순간/the-moment-we-meet.cpp
@@ -33,23 +33,26 @@ int main() {
     }
     //시간 배열 초기화(a,b배열)
     vector<int> aTimeArr(totalTime);
-    int aInitialPos=0,bInitialPos=0;
+    int aPos=0,bPos=0;
+    int aTimeIndex=0,bTimeIndex=0;
     vector<int> bTimeArr(totalTime);
     //a,b모두(다른for문) 시간배열 꺼내보며,해당 시간수만큼 이동. (이동후좌표는
     //시간배열에 저장)
     //a
     for(const auto& val:t){
-        for(int i=0;i<val;i++){
-            aTimeArr[i]=aInitialPos + (1*(d.front()=='L'?-1:1));
-            d.pop();
+        for(int i=aTimeIndex;i<val;i++,aTimeIndex++){
+            aPos = aPos + (1*(d.front()=='L'?-1:1));
+            aTimeArr[i]= aPos;   
         }
+        d.pop();
     }
     //b
     for(const auto& val:t2){
-        for(int i=0;i<val;i++){
-            bTimeArr[i]=bInitialPos + (1*(d.front()=='L'?-1:1));
-            d2.pop();
+        for(int i=bTimeIndex;i<val;i++,bTimeIndex++){
+            bPos = bPos + (1*(d.front()=='L'?-1:1));
+            bTimeArr[i]=bPos;
         }
+        d2.pop();
     }
     //결과: ab배열 순회하여,최초로 값이 같아지는 곳에서 시간 프린트.
     for(int i=0;i<totalTime;i++){
