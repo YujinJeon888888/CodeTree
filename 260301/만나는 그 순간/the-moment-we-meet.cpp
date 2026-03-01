c1781ec253c67cea3fd941ebc58599087829301f
Jeon Yujin <117812553+YujinJeon888888@users.noreply.github.com>
2026-03-01 21:34:51 +0900

    Title: 만나는 그 순간, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

--- 260301/만나는 그 순간/the-moment-we-meet.cpp
+++ 260301/만나는 그 순간/the-moment-we-meet.cpp
@@ -1,62 +1,63 @@
-#include <iostream>
-#include <queue>
 #include <vector>
+#include <queue>
 using namespace std;
 
-int n, m;
+int n,m;
 queue<char> d;
-
+int t[1000];
 queue<char> d2;
+int t2[1000];
 
+int main(){
+    cin>>n>>m;
 
-int main() {
-    cin >> n >> m;
-    vector<int> t(n);
-    vector<int> t2(m);
-    for (int i = 0; i < n; i++) {
+    for(int i=0;i<n;i++){
         char direction;
-        cin >> direction;
+        cin>>direction;
         d.push(direction);
-        cin >> t[i];
-    
+        cin>>t[i];
     }
-    for (int i = 0; i < m; i++) {
+    for(int i=0;i<m;i++){
         char direction;
         cin>>direction;
         d2.push(direction);
-        cin >> t2[i];    
+        cin>>t2[i];
     }
 
-    // Please write your code here.
-    //총 몇시간인지 구하가
+    //총 시간 구하기
     int totalTime=0;
-    for(const auto& val:t){
-        totalTime+=val;
+    for(int i=0;i<n;i++){
+        totalTime+=t[i];
     }
-    //시간 배열 초기화(a,b배열)
+
+    //시간배열초기화
     vector<int> aTimeArr(totalTime);
+    vector<int> bTimeArr(totalTime);
+
     int aPos=0,bPos=0;
     int aTimeIndex=0,bTimeIndex=0;
-    vector<int> bTimeArr(totalTime);
-    //a,b모두(다른for문) 시간배열 꺼내보며,해당 시간수만큼 이동. (이동후좌표는
-    //시간배열에 저장)
-    //a
-    for(const auto& val:t){
-        for(int i=aTimeIndex;i<val;i++,aTimeIndex++){
-            aPos = aPos + (1*(d.front()=='L'?-1:1));
-            aTimeArr[i]= aPos;   
+
+    //이동
+    for(int i=;i<n;i++){
+        int val=t[i];
+
+        for(int step=0;step<val;step++){
+            aPos+=(d.front()=='L'?-1:1);
+            aTimeArr[aTimeIndex++]=aPos;
         }
         d.pop();
     }
-    //b
-    for(const auto& val:t2){
-        for(int i=bTimeIndex;i<val;i++,bTimeIndex++){
-            bPos = bPos + (1*(d2.front()=='L'?-1:1));
-            bTimeArr[i]=bPos;
+    for(int i=0;i<m;i++){
+        int val=t2[i];
+
+        for(int step=0;step<val;step++){
+            bPos+=(d2.front()=='L'?-1:1);
+            bTimeArr[bTimeIndex++]=bPos;
         }
         d2.pop();
     }
-    //결과: ab배열 순회하여,최초로 값이 같아지는 곳에서 시간 프린트.
+
+    //결과출력
     for(int i=0;i<totalTime;i++){
         if(aTimeArr[i]==bTimeArr[i]){
             cout<<i+1;
