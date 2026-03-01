34f4324265df99408fea6e2adb977963b44f23cf
Jeon Yujin <117812553+YujinJeon888888@users.noreply.github.com>
2026-03-01 21:01:16 +0900

    Title: 연속되는 수 3, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260301/연속되는 수 3/continuous-number3.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int N;
+
+
+int main() {
+    cin >> N;
+    vector<int> arr(N);
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+
+    // Please write your code here.
+    int count=1;
+    int maxCount=1;
+    for(int i=1;i<N;i++){
+        if((arr[i]<0&&arr[i-1]<0)||(arr[i]>0&&arr[i-1]>0)){
+            count++
+        }
+        else{
+            count=1;
+        }
+        maxCount=max(maxCount,count);
+    }
+    cout<<maxCount;
+    return 0;
+}
