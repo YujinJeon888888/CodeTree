a3760b2144f2a59cab20dafc064149c9d87f8ca1
Jeon Yujin <117812553+YujinJeon888888@users.noreply.github.com>
2026-03-03 13:01:08 +0900

    Title: 문자에 따른 명령 2, Time: 9ms, Memory: 0MB, Status: Passed - Codetree

--- 260303/문자에 따른 명령 2/text-based-commands2.cpp
+++ 260303/문자에 따른 명령 2/text-based-commands2.cpp
@@ -3,6 +3,9 @@
 using namespace std;
 
 string dirs;
+//동남서북
+int dx[4] = {1,0,-1,0};
+int dy[4] = {0,-1,0,1};
 
 //동남서북으로 정의되어있다고 가정
 void Rotate(char direction, int &dirIndex){
@@ -24,9 +27,6 @@ int main() {
     cin >> dirs;
 
     // Please write your code here.
-    //동남서북
-    int dx[4] = {1,0,-1,0};
-    int dy[4] = {0,-1,0,1};
     int dirIndex = 3;
     int x = 0, y = 0;
     for(const auto& val:dirs){
