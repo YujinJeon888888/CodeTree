0d56b54daca787fc06278aa6eee66a46932863f2
Jeon Yujin <117812553+YujinJeon888888@users.noreply.github.com>
2026-03-03 13:21:00 +0900

    Title: 1이 3개 이상 있는 위치, Time: 11ms, Memory: 0MB, Status: Passed - Codetree

--- 260303/1이 3개 이상 있는 위치/place-more-than-3-ones.cpp
+++ 260303/1이 3개 이상 있는 위치/place-more-than-3-ones.cpp
@@ -4,6 +4,9 @@ using namespace std;
 
 int n;
 int grid[100][100];
+//동남서북
+int dx[4]={1,0,-1,0};
+int dy[4]={0,-1,0,1};
 
 //범위 확인
 bool CheckIsInRange(int x,int y){
@@ -17,32 +20,31 @@ bool CheckIsInRange(int x,int y){
 }
 
 //동남서북에 1있는지 확인 & 카운트 리턴
-int Check1Count(int& grid){
+int Check1Count(int row, int col){
     int count=0;
-    for(int row=0;row<n;row++){
-        for(int col=0;col<n;col++){
-            //동남서북에 대해서 
-            for(int i=0;i<4;i++){
-                //범위체크
-                if(CheckIsInRange(row+i,col+i)==false){
-                    continue;
-                }
-                //1인지 확인
-                if(grid[row+i][col+i]==1){
-                    count++;
-                }
-            }
+    
+    //동남서북에 대해서 
+    for(int i=0;i<4;i++){
+        int nextRow = row + dx[i];
+        int nextCol = col + dy[i];
+
+        //범위체크
+        if(CheckIsInRange(nextRow,nextCol)==false){
+            continue;
+        }
+        //1인지 확인
+        if(grid[nextRow][nextCol]==1){
+            count++;
         }
     }
+      
     return count;
 }
 
-//동남서북
-int dx[4]={1,0,-1,0};
-int dy[4]={0,-1,0,1};
 
 int main() {
     cin >> n;
+    int result = 0;
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
@@ -51,10 +53,10 @@ int main() {
     }
 
     // Please write your code here.
-    int result = 0;
-    for(const auto& row:grid){
-        for(const auto& col: row){
-            if(Check1Count(grid)>=3){
+    
+    for(int row=0;row<n;row++){
+        for(int col=0;col<n;col++){
+            if(Check1Count(row,col)>=3){
                 result++;
             }
         }
