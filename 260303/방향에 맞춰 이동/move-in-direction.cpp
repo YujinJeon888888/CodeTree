6f1e1c14780e026100e4605b3df010623d837273
Jeon Yujin <117812553+YujinJeon888888@users.noreply.github.com>
2026-03-03 12:47:26 +0900

    Title: 방향에 맞춰 이동, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260303/방향에 맞춰 이동/move-in-direction.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+char dir[100];
+int dist[100];
+
+int main() {
+    //동남서북
+    int dx[4] = {1,0,-1,0};
+    int dy[4] = {0,-1,0,1};
+    int x=0,y=0;//current
+    int dirIndex = 0; 
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> dir[i] >> dist[i];
+        if(dir[i]=='E'){
+            dirIndex=0;
+        }
+        if(dir[i]=='S'){
+            dirIndex=1;
+        }
+        if(dir[i]=='W'){
+            dirIndex=2;
+        }
+        if(dir[i]=='N'){
+            dirIndex=3;
+        }
+        
+        x+=dx[dir];
+        y+=dy[dir];
+    }
+
+    // Please write your code here.
+    cout<<x<<' '<<y;
+    return 0;
+}
