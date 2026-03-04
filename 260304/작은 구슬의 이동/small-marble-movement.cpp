52897e6c8d87c59d0d6449c47d34c7f26a80f2e7
Jeon Yujin <117812553+YujinJeon888888@users.noreply.github.com>
2026-03-04 12:18:09 +0900

    Title: 작은 구슬의 이동, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260304/작은 구슬의 이동/small-marble-movement.cpp
+++ 260304/작은 구슬의 이동/small-marble-movement.cpp
@@ -1,98 +1,64 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
-int n, t;
-int r, c;
+int n,t;
+int r,c;
 char d;
-//동북서남
-int dx[4] = {1,0,-1,0};
-int dy[4] = {0,1,0,-1};
 
-//방향플립
-int IfHitReturnNewDirIndex(int& dirIndex){
-    
-    if(dirIndex==0){
-        dirIndex= 2;
-        return dirIndex;
-    }
-    else if(dirIndex==1){
-        dirIndex= 3;
-        return dirIndex;
-    }
-    else if(dirIndex==3){
-        dirIndex= 1;
-        return dirIndex;
-    }
-    else if(dirIndex==2){
-        dirIndex= 0;
-        return dirIndex;
-    }
+//동남서북
+int dRow[4]={0,-1,0,1};
+int dCol[4]={1,0,-1,0};
+
+//방향 플립
+int IfHitReturnNewDirIndex(int dirIndex){
+    //동남서북
+    if(dirIndex==0) return 2;
+    if(dirIndex==1) return 3;
+    if(dirIndex==2) return 0;
+    if(dirIndex==3) return 1;
     return dirIndex;
 }
 
-//범위 내 있는지
-bool IsInRange(int row, int col){
-    if(row<=0||row>n){
-        return false;
-    }
-    if(col<=0||col>n){
-        return false;
-    }
+//범위체크
+bool IsInRange(int row,int col){
+    if(row<=0||row>n) return false;
+    if(col<=0||col>n) return false;
     return true;
 }
+
 //이동
-void Move(vector<vector<int>> &board, int& dirIndex, int& x, int& y){
-    //이전위치 캐시
-    int befX=x;
-    int befY=y;
-    //새로운 위치 구하기
-    int tempX = x+dx[dirIndex];
-    int tempY = y+dy[dirIndex];
-    //만약 범위를 벗어나면 
+void Move(int& dirIndex, int& x, int& y){
+    int tempX=x+dCol[dirIndex];
+    int tempY=y+dRow[dirIndex];
+
     if(IsInRange(tempX,tempY)==false){
-        //플립 후 이동 x
-        dirIndex = IfHitReturnNewDirIndex(dirIndex);
+        dirIndex=IfHitReturnNewDirIndex(dirIndex);
     }
     else{
-        //이동 후 이동위치 기록(1)
         x=tempX;
         y=tempY;
-        board[x][y] = 1;
-        //이전위치 지우기 (0)
-        board[befX][befY] = 0;
-    }        
+    }
 }
 
+int main(){
+    cin>>n>>t;
+    cin>>r>>c>>d;
 
-int main() {
-    cin >> n >> t;
-    cin >> r >> c >> d;
     int x=r;
     int y=c;
     int dirIndex=0;
-    // Please write your code here.
-    //보드판 초기화 (1이면 바둑판)
-    vector<vector<int>> board(n,vector<int>(n+1,0));
-    //방향 정하기
-    if(d=='L'){
-        dirIndex=2;
-    }
-    else if(d=='R'){
-        dirIndex=0;
-    }
-    else if(d=='U'){
-        dirIndex=1;
-    }
-    else if(d=='D'){
-        dirIndex=3;
-    }
 
-    //초마다 이동하기
+    //방향
+    if(d=='R') dirIndex=0;
+    else if(d=='D') dirIndex=1;
+    else if(d=='L') dirIndex=2;
+    else if(d=='U') dirIndex=3;
+
+    //초마다 이동
     for(int i=0;i<t;i++){
-        Move(board,dirIndex,x,y);
+        Move(dirIndex,x,y);
     }
 
-    cout<<x+1<<' '<<y+1;
+    cout<<x<<' '<<y;
     return 0;
 }
