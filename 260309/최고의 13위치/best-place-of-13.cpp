79e867cf8eb7340f4d804d091a23bd1151c2738f
Jeon Yujin <117812553+YujinJeon888888@users.noreply.github.com>
2026-03-09 15:05:37 +0900

    Title: 최고의 13위치, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260309/최고의 13위치/best-place-of-13.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int N;
+int grid[20][20];
+
+int main() {
+    cin >> N;
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++) cin >> grid[i][j];
+
+    // Please write your code here.
+    int maxCount=0;
+    // for: 행
+    for(int row=0;row<N;row++){
+        //for: 열(최대: -2까지)
+        for(int col=0;col<N-2;col++){
+            //grid[i][j]+grid[i][j+1]+grid[i][j+2]가 max인지 확인하여 맥시멈갱신
+            maxCount=max(maxCount,grid[i][j]+grid[i][j+1]+grid[i][j+2]);
+        }
+    }
+    cout<<maxCount;   
+            
+        
+    return 0;
+}
