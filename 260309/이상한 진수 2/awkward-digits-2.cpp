786abf9dc17d876a790dfc4df9366b7e1ce64d0e
Jeon Yujin <117812553+YujinJeon888888@users.noreply.github.com>
2026-03-09 16:20:07 +0900

    Title: 이상한 진수 2, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260309/이상한 진수 2/awkward-digits-2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string a;
+
+int main() {
+    cin >> a;
+
+    // Please write your code here.
+    // for: 0인지 확인
+    for(int i=0;i<a.length();i++){
+        //0이면: 플립
+        if(a[i]=='0'){
+            a[i]='1';
+            break;
+        }
+        //전부 1이면: 끝 플립
+        if(i==a.length()-1&&a[i]==1){
+            a[i]=0;
+        }
+    }
+        
+
+    //2진수를 10진수로
+    int result = stoi(a,nullptr,2);
+    cout<<result;
+    
+    return 0;
+}
