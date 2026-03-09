442f5e70f65d2a077a8de3bc6f7a2f268aa8b0d3
Jeon Yujin <117812553+YujinJeon888888@users.noreply.github.com>
2026-03-09 16:06:36 +0900

    Title: 체크판위에서 2, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260309/체크판위에서 2/on-the-checkboard-2.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+#define MAX_N 15
+
+using namespace std;
+
+int n,m;
+char grid[MAX_N][MAX_N];
+
+int main() {
+    //입력
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> grid[i][j];
+        }
+    }
+
+    // 이동 시에 행과 열이 전부 증가하도록
+    // 모든 쌍을 다 잡아보기
+    int cnt=0;
+    for(int i=1;i<n-2;i++){
+        for(int j=1;j<m-2;j++){
+            for(int k=i+1;k<n-1;k++){
+                for(int l=j+1;l<m-1;l++){
+                    //그 중 색깔이 전부 달라지는 경우에만 개수세기
+                    if(grid[0][0]!=grid[i][j]&&
+                    grid[i][j]!=grid[k][l]&&
+                    grid[k][l]!=grid[n-1][m-1]){
+                        cnt++;
+                    }
+                }
+            }
+        }
+    }
+
+    cout<<cnt;
+
+    return 0;
+}
