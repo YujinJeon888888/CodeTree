4b74432aac7744e4c74f37df6ecf05dce4524715
Jeon Yujin <117812553+YujinJeon888888@users.noreply.github.com>
2026-03-12 14:19:02 +0900

    Title: Carry 피하기 2, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 260312/Carry 피하기 2/escaping-carry-2.cpp
+++ 260312/Carry 피하기 2/escaping-carry-2.cpp
@@ -1,81 +1,30 @@
 #include <iostream>
-#include <climits>//INT_MIN
-#include <algorithm>//min
+#include <climits>
+#include <algorithm>
+#include <vector>
+
 using namespace std;
 
 int n;
 int arr[20];
 
-bool IsCarry(int a,int b){
-    //for: 10으로 나눈 후 몫 구하기 
-    while(true){
-        int aShare=a/10;
-        a/=10;
-        int bShare=b/10;
-        b/=10;
-        //(만약 몫이 0이면 10으로 나머지 구하고 나머지끼리 더하여 결과낸 뒤 break)
-        if(aShare==0||bShare==0){
-            int aRemainder=a%10;
-            int bRemainder=b%10;
-
-            if(aRemainder+bRemainder>=10){
-                return true;
-            }
-
-            break;
-        }
-        //몫*10으로 나머지 구하기 
-        int divider=a<b?aShare*10:bShare*10;
-        int aRemainder=a%divider;
-        int bRemainder=b%divider;
-        //나머지끼리 더하기
-        //만약 10을 넘어가면 return true;
-        if(aRemainder+bRemainder>=10){
-            return true;
-        }
-    }
-    //return false;
-    return false;
-}
-
 bool IsCarry(int a,int b, int c){
-    //for: 10으로 나눈 후 몫 구하기 
-    while(true){
-        int aShare=a/10;
-        a/=10;
-        int bShare=b/10;
-        b/=10;
-        int cShare=c/10;
-        c/=10;
-        //(만약 몫이 0이면 10으로 나머지 구하고 나머지끼리 더하여 결과낸 뒤 break)
-        if(aShare==0||bShare==0||cShare==0){
-            int aRemainder=a%10;
-            int bRemainder=b%10;
-            int cRemainder=c%10;
+    //자릿수가 남은 동안 반복
+    while(a>0||b>0||c>0){
+        //끝자리수 더해보기
+        int sumDigits= (a%10) + (b%10) + (c%10);
 
-            if(aRemainder+bRemainder+cRemainder>=10){
-                return true;
-            }
-
-            break;
-        }
-        //몫*10으로 나머지 구하기 
-        int minShare=min({aShare,bShare,cShare});
-        int divider=minShare*10;
-        int aRemainder=a%divider;
-        int bRemainder=b%divider;
-        int cRemainder=c%divider;
-        //나머지끼리 더하기
-        //만약 10을 넘어가면 return true;
-        if(aRemainder+bRemainder+cRemainder>=10){
+        //합이 10 이상인지 확인
+        if(sumDigits>=10) {
             return true;
         }
+        //다음 자릿수로 넘어가기 위해 10으로 나누기
+        a/=10;
+        b/=10;
+        c/=10;
     }
-    //return false;
     return false;
 }
-
-
 
 int main() {
     cin >> n;
@@ -84,36 +33,19 @@ int main() {
     }
 
     // Please write your code here.
-    int maxNum=INT_MIN;
-    //for: 숫자 1
-    for(int i=0;i<n-2;i++){
-        //for: 숫자 2
-        for(int j=i+1;j<n-1;j++){
-            //각 자리수를 더해서, 캐리가 발생하면 continue;
-            if(IsCarry(arr[i],arr[j])){
-                continue;
-            }
-            //for: 숫자 3
+    int maxTotal=-1;
+
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
             for(int k=j+1;k<n;k++){
-                //각 자리수를 더해서, 캐리가 발생하면 continue;
-                if(IsCarry(arr[i],arr[j],arr[k])){
-                    continue;
-                }   
-                //합 더하기
-                int sum=arr[i]+arr[j]+arr[k];
-                //max갱신
-                maxNum=max(maxNum,sum);
+                if(IsCarry(arr[i],arr[j],arr[k])==false){
+                    maxTotal=max(maxTotal,arr[i]+arr[j]+arr[k]);
+                }
             }
         }
     }
-    if(maxNum==INT_MIN){
-        cout<<-1;
-        return 0;
-    }
-    
-    cout<<maxNum;
-            
-            
+
+    cout<<maxTotal;
 
     return 0;
 }
