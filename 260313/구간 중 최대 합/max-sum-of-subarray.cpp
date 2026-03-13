94b772437742fbf29f51ec37d93e0aee010c52f8
Jeon Yujin <117812553+YujinJeon888888@users.noreply.github.com>
2026-03-13 14:15:33 +0900

    Title: 구간 중 최대 합, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260313/구간 중 최대 합/max-sum-of-subarray.cpp
+++ 260313/구간 중 최대 합/max-sum-of-subarray.cpp
@@ -14,7 +14,7 @@ int main() {
     // Please write your code here.
     int answer=-1;
     //for: 시작 위치
-    for(int i=0;i+k<n;i++){
+    for(int i=0;i+k<=n;i++){
         //for: 시작위치부터 끝위치까지 더하기
         int sum=0;
         int index=i;
