7ffed8efdbb8cbdad93940da7c3b157d023eec79
Jeon Yujin <117812553+YujinJeon888888@users.noreply.github.com>
2026-03-13 14:40:27 +0900

    Title: 특정 구간의 원소 평균값, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260313/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int arr[100+1];
+
+int main() {
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        cin >> arr[i];
+    }
+
+    // Please write your code here.
+    int result=0;
+    //for: 시작점
+    for(int i=1;i<=n;i++){
+        //for: 끝점
+        for(int j=i+1;j<=n;j++){
+            // for: 시작점부터 끝점까지 더하고 
+            int sum=0; int avr=0;
+            for(int k=i;k<=j;k++){
+                sum+=arr[k];
+            }
+            
+            //평균내기(int)
+            avr=sum/(j-i+1);
+            //for: 평균이 시작점부터 끝점 중 하나의 원소와 같은지 확인
+            for(int k=i;k<=j;k++){
+                //같으면: result++
+                if(arr[k]==avr){
+                    result++;
+                    break;
+                }
+            }
+        }
+    }
+        
+            
+
+    //result출력
+    cout<<result;
+    return 0;
+}
