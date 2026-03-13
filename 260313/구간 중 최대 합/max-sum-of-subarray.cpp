742576e3ee46fb8ded4f77d54f8f9aea2c9ddad5
Jeon Yujin <117812553+YujinJeon888888@users.noreply.github.com>
2026-03-13 14:09:57 +0900

    Title: 구간 중 최대 합, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260313/구간 중 최대 합/max-sum-of-subarray.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n, k;
+int arr[100];
+
+int main() {
+    cin >> n >> k;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    // Please write your code here.
+    int answer=-1;
+    //for: 시작 위치
+    for(int i=0;i+k-1<n;i++){
+        //for: 시작위치부터 끝위치까지 더하기
+        int sum=0;
+        for(int j=i;j+k-1<n;j++){
+            //sum구하기
+            sum+=arr[j];
+        }
+        //최대 sum갱신 
+        answer=max(answer,sum);
+
+    }
+        
+    cout<<answer;           
+        
+    return 0;
+}
