7ec8f4aa06fc60384958ab2ac318a1888ddbd681
Jeon Yujin <117812553+YujinJeon888888@users.noreply.github.com>
2026-03-16 14:29:12 +0900

    Title: 한 가지로 열리는 자물쇠, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260316/한 가지로 열리는 자물쇠/one-way-lock.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cmath>//abs
+
+using namespace std;
+
+int N;
+int a, b, c;
+
+int main() {
+    cin >> N;
+    cin >> a >> b >> c;
+
+    // Please write your code here.
+    int count=0;
+    //for: 일의자리
+    for(int i=1;i<=N;i++){
+        //for: 십의자리
+        for(int j=1;j<=N;j++){
+            //for: 백의자리
+            for(int k=1;k<=N;k++){
+                if(abs(a-i)<=2|| abs(a-j)<=2|| abs(a-k)<=2 
+                ||abs(b-i)<=2|| abs(b-j)<=2|| abs(b-k)<=2 
+                ||abs(c-i)<=2|| abs(c-j)<=2|| abs(c-k)<=2 )
+                    count++;  
+            }        
+        }
+    }
+    cout<<count;
+            
+    return 0;
+}
