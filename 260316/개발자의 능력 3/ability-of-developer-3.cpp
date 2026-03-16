7e530a75a54f4d7dd36c77c1c803f0b1f62b65de
Jeon Yujin <117812553+YujinJeon888888@users.noreply.github.com>
2026-03-16 14:45:02 +0900

    Title: 개발자의 능력 3, Time: 9ms, Memory: 0MB, Status: Passed - Codetree

--- 260316/개발자의 능력 3/ability-of-developer-3.cpp
+++ 260316/개발자의 능력 3/ability-of-developer-3.cpp
@@ -6,10 +6,14 @@ using namespace std;
 
 int abilities[6];
 //팀원1,팀원2,팀원3을 인자로 넣기
-int GetDiff(int teammate1, int teammate2, int teammate3){
+int GetDiff(int teammate1Index, int teammate2Index, int teammate3Index){
     int sum1=0, sum2=0;
-    sum1=teammate1+teammate2+teammate3;
+    sum1=abilities[teammate1Index]+abilities[teammate2Index]+abilities[teammate3Index];
+
     for(int i=0;i<6;i++){
+        if(i==teammate1Index||i==teammate2Index||i==teammate3Index){
+            continue;
+        }
         sum2+=abilities[i];
     }
     return abs(sum2-sum1);
@@ -29,8 +33,12 @@ int main() {
         for(int j=0;j<6;j++){
             //for: 팀원3
             for(int k=0;k<6;k++){
+                //팀원이 다 달라야 함
+                if(i==j||j==k||k==i){
+                    continue;
+                }
                 //getDiff하기
-                int diff=GetDiff(abilities[i],abilities[j],abilities[k]);
+                int diff=GetDiff(i,j,k);
                 //if(getDiff<minResult)
                 if(diff<minResult){
                     //갱신
@@ -39,6 +47,7 @@ int main() {
             }
         }
     }
+
     cout<<minResult;
 
     return 0;
