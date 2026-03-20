c20fdf461a3e1db371e0226a2f304f536d185b67
Jeon Yujin <117812553+YujinJeon888888@users.noreply.github.com>
2026-03-20 22:32:59 +0900

    Title: 문자열 범위 출력하기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260320/문자열 범위 출력하기/print-string-in-range.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s;
+    // Please write your code here.
+    getLine(cin,s);
+    for(int i=2;i<9;i++){
+        cout<<s[i];
+    }
+    return 0;
+}
