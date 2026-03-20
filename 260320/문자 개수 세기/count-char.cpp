1eef0a55d6c88816dfac522dbcb29e2c1597dfab
Jeon Yujin <117812553+YujinJeon888888@users.noreply.github.com>
2026-03-20 22:39:43 +0900

    Title: 문자 개수 세기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260320/문자 개수 세기/count-char.cpp
+++ 260320/문자 개수 세기/count-char.cpp
@@ -6,11 +6,13 @@ int main() {
     // Please write your code here.
     string s;
     getline(cin,s);
+    char c;
+    cin>>c;
 
     int count=0;
     
     for(const auto& val:s){
-        if(val>='a'&&val<='z'){
+        if(val==c){
             count++;
         }
     }
