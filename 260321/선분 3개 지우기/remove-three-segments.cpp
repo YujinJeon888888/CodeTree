62eac959dfe44a667ad070f3b0f60a4ea0a7a5de
Jeon Yujin <117812553+YujinJeon888888@users.noreply.github.com>
2026-03-21 16:42:55 +0900

    Title: 선분 3개 지우기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260321/선분 3개 지우기/remove-three-segments.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#define MAX_NUM 100
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int ans=0;
+    // 입력받기
+    int n;
+    cin>>n;
+    vector<int> line(100+1,0);
+    //시작,끝 인덱스
+    vector<pair<int,int>> lines(n);
+    for(int i=0;i<n;i++){
+        int a,b;
+        cin>>a>>b;
+        for(int j=a;j<=b;j++){
+            line[j]++;
+            lines.push_back({a,b});
+        }
+    }
+    //선분1
+    for(int i=0;i<n;i++){
+        //선분2
+        for(int j=i+1;j<n;j++){
+            //선분3
+            for(int k=j+1;k<n;k++){
+                //temp에 line복제
+                auto temp = line;
+                //temp에서 선분1,2,3제거
+                for(int index=temp[i].first;index<=temp[i].second;index++){
+                    temp[index]--;    
+                }
+                for(int index=temp[j].first;index<=temp[j].second;index++){
+                    temp[index]--;    
+                }
+                for(int index=temp[k].first;index<=temp[k].second;index++){
+                    temp[index]--;    
+                }
+                //for: 겹치는지 확인
+                bool isOvelapped=false;
+                for(const auto& val:temp){
+                    if(val>=2){
+                        isOverlapped=true;
+                        break;
+                    }
+                }
+                //안겹치면 ans++   
+                if(!isOverlapped){
+                    ans++;
+                }
+            }
+        }            
+    }
+    cout<<ans;
+        
+    
+    return 0;
+}
