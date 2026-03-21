79937eeaac77c0f81c0b4b36797e8ac9f381437e
Jeon Yujin <117812553+YujinJeon888888@users.noreply.github.com>
2026-03-21 16:26:40 +0900

    Title: 이상한 폭탄 2, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260321/이상한 폭탄 2/strange-bomb-2.cpp
+++ 260321/이상한 폭탄 2/strange-bomb-2.cpp
@@ -20,25 +20,29 @@ int main() {
     //폭탄 번호 고르기
     for(int bombNum=0;bombNum<=1000;bombNum++){
         int bombCount=0;
-        //arr순회: 같은 폭탄 번호면 이걸 시작점으로하고 break;
         int startIndex=-1;
+        //arr순회: 같은 폭탄 번호면 이걸 시작점으로
         for(int i=0;i<n;i++){
             if(bombs[i]==bombNum){
                 startIndex=i;
             }
         }
-        
+        //그 폭탄 없으면 continue
         if(startIndex==-1){
             continue;
         }
 
-        //시작점 k만큼 순회: 같은 폭탄 번호 있으면
+        //시작점부터 k만큼 순회: 같은 폭탄 번호 있으면
         int iterateNum=k;
         int index=startIndex;
         while(iterateNum>0){
             iterateNum--;
             index++;
-
+            //인덱스 초과 확인
+            if(index>=n){
+                break;
+            }
+            //터지면 결과에 insert
             if(bombs[index]==bombNum){
                 ans.insert(bombNum);
                 break;
@@ -46,7 +50,7 @@ int main() {
         }
     }
 
-    if(set.size()==0){
+    if(ans.size()==0){
         cout<<-1;    
     }
     else{
