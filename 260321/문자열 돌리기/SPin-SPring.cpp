cfd9434dbe3f8e300aaacbfe7089215f0b8ab054
Jeon Yujin <117812553+YujinJeon888888@users.noreply.github.com>
2026-03-21 11:58:06 +0900

    Title: 문자열 돌리기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260321/문자열 돌리기/SPin-SPring.cpp
+++ 260321/문자열 돌리기/SPin-SPring.cpp
@@ -7,12 +7,12 @@ int main() {
     string s;
     cin>>s;
 
-    cout<<s;
+    cout<<s<<'\n';
 
-    //첫 문자열 떼고 맨 뒤로 붙여서 재조합하는 것 반복
+    //맨뒤 문자열 떼고 맨 앞으로 붙여서 재조합하는 것 반복
     for(int i=0;i<s.length();i++){
         s=s.substr(s.length()-1,1)+s.substr(0,s.length()-1);
-        cout<<s;
+        cout<<s<<'\n';
     }
     return 0;
 }
